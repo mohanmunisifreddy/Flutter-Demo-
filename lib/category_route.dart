fa4850c28a9dddea9e37f8ce3beb53d16bd2d91b
Mohan Munisifreddy <mohanmunisifreddy@gmail.com>
2019-04-14 23:40:23 +0530

    Added listview

--- lib/category_route.dart
+++ lib/category_route.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hello_rectangle/category.dart';
+
+final _backgroundColor = Colors.green[100];
 
 class CategoryRoute extends StatelessWidget {
   const CategoryRoute();
@@ -24,14 +27,78 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+        itemBuilder: (BuildContext context, int index) => categories[index],
+        itemCount: categories.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final listView = Container();
-    final appBar = AppBar();
+    final categories = <Category>[];
+    for(var i=0; i<_categoryNames.length; i++) {
+      categories.add(
+        Category(
+          name: _categoryNames[i],
+          color: _baseColors[i],
+          iconLocation: Icons.cake,
+        )
+      );
+    }
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal:8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+
+    final appBar = AppBar(
+      elevation: 0.0,
+      title: Text(
+        "Unit Converter",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
+
     return Scaffold(
       appBar: appBar,
       body: listView,
     );
   }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_rectangle/category.dart';
-
-const _categoryName = "Cake";
-const _categoryIcon = Icons.cake;
-const _categoryColor = Colors.green;
-var bgColor = Colors.green[100];
+import 'package:hello_rectangle/category_route.dart';
 
 void main() {
   runApp(UnitConverterApp());
@@ -16,22 +11,7 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Unit Converter", style: TextStyle(fontSize: 30.0, color: Colors.black),),
-          backgroundColor: bgColor,
-          elevation: 0.0,
-          centerTitle: true,
-        ),
-        backgroundColor: bgColor,
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon
-          ),
-        ),
-      ),
+      home: CategoryRoute(),
     );
   }
 }
